16ddb54b434d601cad4c2b880ffdee6208b7a0e8
revolverocelot1 <srushtiraj.patil20@vit.edu>
2025-06-13 19:38:00 +0530

    fixed the google issue again

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="text-center py-8 border-t border-solid border-t-[#223649] mt-auto">
-      <p className="text-sm text-[var(--text-secondary)]">© 2024 Anemoia. All rights reserved.</p>
+    <footer className="w-full text-center py-8 border-t border-solid border-t-[#223649] mt-auto">
+      <p className="text-sm text-[var(--text-secondary)]">© 2025 Anemoia. All rights reserved.</p>
     </footer>
   );
 };
