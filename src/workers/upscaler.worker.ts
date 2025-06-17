be5a1188faa354dc4314a3f59a69cc530779ffd4
revolverocelot1 <srushtiraj.patil20@vit.edu>
2025-06-17 17:08:33 +0530

    fix(upscaler): use working GitHub raw URLs from xororz/web-realesrgan for Real-ESRGAN/Real-CUGAN models

--- src/workers/upscaler.worker.ts
+++ src/workers/upscaler.worker.ts
@@ -8,13 +8,13 @@ const MODEL_CONFIGS = {
     name: 'Real-CUGAN x2 Conservative',
     scaleFactor: 2,
     tileSize: 64,
-    modelPath: 'https://cdn.jsdelivr.net/npm/@upscalerjs/esrgan-thick@1.0.0-beta.16/models/2x/model.json',
+    modelPath: 'https://raw.githubusercontent.com/xororz/web-realesrgan/master/public/models/cugan_2x_conservative_64/model.json',
   },
   x4: {
     name: 'Real-ESRGAN x4 General+',
     scaleFactor: 4,
     tileSize: 64,
-    modelPath: 'https://cdn.jsdelivr.net/npm/@upscalerjs/esrgan-thick@1.0.0-beta.16/models/4x/model.json',
+    modelPath: 'https://raw.githubusercontent.com/xororz/web-realesrgan/master/public/models/realesrgan_x4_general_plus_64/model.json',
   },
 };
 
